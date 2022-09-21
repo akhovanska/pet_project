6a17ce09873b3f62ca52effc79510929ce09a18a
Anastasiia Khovanska <98605728+akhovanska@users.noreply.github.com>
2022-09-21 11:44:15 +0300

    Create main.py

---
+++ snake_game/main.py
@@ -0,0 +1,87 @@
+import math
+import random
+import pygame
+import random
+import tkinter as tk
+from tkinter import messagebox
+from classes import snake
+from classes import cube
+
+
+width = 500
+height = 500
+
+cols = 25
+rows = 20
+
+
+
+def redrawWindow():
+    global win
+    win.fill((0, 0, 0))
+    drawGrid(width, rows, win)
+    s.draw(win)
+    snack.draw(win)
+    pygame.display.update()
+    pass
+
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
+
+
+def randomSnack(rows, item):
+    positions = item.body
+
+    while True:
+        x = random.randrange(1, rows - 1)
+        y = random.randrange(1, rows - 1)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
+
+    return (x, y)
+
+
+def main():
+    global s, snack, win
+    win = pygame.display.set_mode((width, height))
+    s = snake((255, 0, 0), (10, 10))
+    s.addCube()
+    snack = cube(randomSnack(rows, s), color=(0, 255, 0))
+    flag = True
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(30)
+        clock.tick(10)
+        s.move()
+        headPos = s.head.pos
+        if headPos[0] >= 20 or headPos[0] < 0 or headPos[1] >= 20 or headPos[1] < 0:
+            print("Score:", len(s.body))
+            s.reset((10, 10))
+
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            snack = cube(randomSnack(rows, s), color=(0, 255, 0))
+
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z: z.pos, s.body[x + 1:])):
+                print("Score:", len(s.body))
+                s.reset((10, 10))
+                break
+
+        redrawWindow()
+
+
+main()
